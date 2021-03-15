1ed246d488c7a40e17c58ae232864d21980f6068
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2021-03-15 12:38:49 -0300

    populando publisher e developer dos games

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -6,6 +6,7 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -23,12 +24,30 @@ async function getGameInfo(slug) {
   }
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true })
+    });
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
 
     const { data: { products } } = await axios.get(gogApiUrl);
 
-    console.log(await getGameInfo(products[0].slug));
+    await create(products[1].publisher, "publisher");
+    await create(products[1].developer, "developer");
   }
 };
