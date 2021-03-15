d733eedbbb0dc6a732a99ae58883f68db891f822
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2021-03-15 14:41:19 -0300

    criacao de funcao para criar games

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-/**
+/*
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
@@ -15,13 +15,13 @@ async function getGameInfo(slug) {
   const body = await axios.get(`https://www.gog.com/game/${slug}`);
   const dom = new JSDOM(body.data);
 
-  const description = dom.window.document.querySelector('.description');
+  const description = dom.window.document.querySelector(".description");
 
   return {
-    rating: 'BR0',
+    rating: "BR0",
     short_description: description.textContent.slice(0, 160),
-    description: description.innerHTML
-  }
+    description: description.innerHTML,
+  };
 }
 
 async function getByName(name, entityName) {
@@ -36,18 +36,82 @@ async function create(name, entityName) {
   if (!item) {
     return await strapi.services[entityName].create({
       name,
-      slug: slugify(name, { lower: true })
+      slug: slugify(name, { lower: true }),
     });
   }
+}
+
+async function createManyToMany(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+
+    developers[developer] = true;
+
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(Number(product.globalReleaseDate) * 1000).toISOString(),
+          categories: await Promise.all(product.genres.map((name) => getByName(name, "category"))),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
 }
 
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
 
-    const { data: { products } } = await axios.get(gogApiUrl);
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
 
-    await create(products[1].publisher, "publisher");
-    await create(products[1].developer, "developer");
-  }
+    await createManyToMany([products[2], products[3]]);
+    await createGames([products[2], products[3]]);
+  },
 };
